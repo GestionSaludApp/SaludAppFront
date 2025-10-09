4c4c9acf1f9db681b90c5be538932adda180b0bb
DaianaBranca <44965096@itbeltran.com.ar>
2025-10-09 19:30:15 -0300

    actualice el boton

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
-import{RouterLink} from '@angular/router';  
+import { Component, OnInit } from '@angular/core';
+import { UsuarioActivoService } from '../../servicios/usuario-activo.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
- 
+export class NavbarComponent implements OnInit {
+
+  sesionActiva = false;
+
+  constructor(private usuarioActivo: UsuarioActivoService, private router: Router) {}
+
+  ngOnInit() {
+    this.usuarioActivo.sesionActiva$.subscribe(estado => {
+      this.sesionActiva = estado;
+    });
+  }
+
+  cerrarSesion() {
+    this.usuarioActivo.limpiarUsuario();
+    this.router.navigate(['/inicio']);
+
+    Swal.fire({
+      title: 'Sesión cerrada',
+      text: 'Has cerrado sesión correctamente.',
+      icon: 'success',
+      confirmButtonText: 'Aceptar',
+      confirmButtonColor: '#0d6efd'
+    });
+  }
+
 }
